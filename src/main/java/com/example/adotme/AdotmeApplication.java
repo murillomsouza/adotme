37917078001e2b5e27d7e16060b6248c97f262bd
murillomsouza <murillosouza997@gamil.com>
2026-04-04 14:15:59 -0300

    Initial commit: estrutura spring boot com MySQL e MongoDB

---
+++ src/main/java/com/example/adotme/AdotmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.adotme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adotme/AdotmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adotme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
